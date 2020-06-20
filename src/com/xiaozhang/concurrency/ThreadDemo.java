c5b48984a2971da4dcc65cef3991d9ed5f1cfa6b
meilixiaozhang <redpangci@gmail.com>
2020-06-20 23:50:49 +1000

    Update ThreadDemo.java

--- src/com/xiaozhang/concurrency/ThreadDemo.java
+++ src/com/xiaozhang/concurrency/ThreadDemo.java
@@ -1,6 +1,7 @@
 package com.xiaozhang.concurrency;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ThreadDemo {
     public static void show() {
@@ -100,28 +101,34 @@ public class ThreadDemo {
 //        }
 //        System.out.println(status.getTotalBytes());
 //      ================================================
-        Collection<Integer> collection = Collections.synchronizedCollection(new ArrayList<>());
-
-        var thread1 = new Thread(()->{
-            collection.addAll(Arrays.asList(1,2,3));
-        });
-        var thread2 = new Thread(()->{
-            collection.addAll(Arrays.asList(4,5,6));
-        });
-
-        thread1.start();
-        thread2.start();
-
-        try {
-            thread1.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        try {
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(collection);
+//      synchronized collection
+//        Collection<Integer> collection = Collections.synchronizedCollection(new ArrayList<>());
+//
+//        var thread1 = new Thread(()->{
+//            collection.addAll(Arrays.asList(1,2,3));
+//        });
+//        var thread2 = new Thread(()->{
+//            collection.addAll(Arrays.asList(4,5,6));
+//        });
+//
+//        thread1.start();
+//        thread2.start();
+//
+//        try {
+//            thread1.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            thread2.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(collection);
+//      ================================================
+        Map<Integer, String> map = new ConcurrentHashMap<>();
+        map.put(1,"a");
+        map.get(1);
+        map.remove(1);
     }
 }
