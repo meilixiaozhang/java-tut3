47da59b2c94e522395bb52345179bce66c52a85e
meilixiaozhang <redpangci@gmail.com>
2020-06-20 20:08:25 +1000

    joining a thread

--- src/com/xiaozhang/concurrency/ThreadDemo.java
+++ src/com/xiaozhang/concurrency/ThreadDemo.java
@@ -7,6 +7,21 @@ public class ThreadDemo {
         for (var i = 0; i < 10; i++) {
             Thread thread = new Thread(new DownloadFileTask());
             thread.start();
+            // block the current threads until the downloading threads has finished.
+//          Downloading a file: Thread-0
+//          wait 5 sec
+//          Download complete.Thread-0
+//          File is read to scan.
+//          Downloading a file: Thread-1
+//          ........
+//          Hold the thread to wait for the completion of another thread, when a thread is waiting, it cannot do other thibgs
+
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("File is read to scan.");
         }
     }
 }
