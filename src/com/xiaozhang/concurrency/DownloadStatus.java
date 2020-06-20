b03ce86680b3bae59ab93faf072fcf3380bdeec5
meilixiaozhang <redpangci@gmail.com>
2020-06-20 22:29:14 +1000

    Lock

--- src/com/xiaozhang/concurrency/DownloadStatus.java
+++ src/com/xiaozhang/concurrency/DownloadStatus.java
@@ -1,13 +1,21 @@
 package com.xiaozhang.concurrency;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
+    private int totalBytes;
+    private Lock lock = new ReentrantLock();
     public int getTotalBytes() {
         return totalBytes;
     }
 
-    private int totalBytes;
-
     public void incrementTotalBytes() {
-        totalBytes++;
+        lock.lock();
+        try {
+            totalBytes++;
+        } finally {
+            lock.unlock();
+        }
     }
 }
