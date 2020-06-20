88cbcbc3e81e8ecf192e70108592a81b0e25d8e2
meilixiaozhang <redpangci@gmail.com>
2020-06-20 20:18:03 +1000

    interrupting a thread

--- src/com/xiaozhang/concurrency/DownloadFileTask.java
+++ src/com/xiaozhang/concurrency/DownloadFileTask.java
@@ -4,11 +4,20 @@ public class DownloadFileTask implements Runnable{
     @Override
     public void run() {
         System.out.println("Downloading a file: " + Thread.currentThread().getName());
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+//      ====================Pausing a thread=======================
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//      ============================================================
+
+        for (var i = 0; i < Integer.MAX_VALUE; i++) {
+            if (Thread.currentThread().isInterrupted())
+                return;
+            System.out.println("Downloading byte " + i);
         }
+
         System.out.println("Download complete." + Thread.currentThread().getName());
     }
 }
--- src/com/xiaozhang/concurrency/ThreadDemo.java
+++ src/com/xiaozhang/concurrency/ThreadDemo.java
@@ -7,21 +7,32 @@ public class ThreadDemo {
         for (var i = 0; i < 10; i++) {
             Thread thread = new Thread(new DownloadFileTask());
             thread.start();
+//          ============ Joining a thread ==================
             // block the current threads until the downloading threads has finished.
 //          Downloading a file: Thread-0
 //          wait 5 sec
 //          Download complete.Thread-0
 //          File is read to scan.
 //          Downloading a file: Thread-1
-//          ........
+//          ...
+//          ...
 //          Hold the thread to wait for the completion of another thread, when a thread is waiting, it cannot do other thibgs
 
+//            try {
+//                thread.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("File is read to scan.");
+//          =================================================
             try {
-                thread.join();
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("File is read to scan.");
+            // does not force the thread to stop, it only sends out an interrupt request
+            thread.interrupt();
+
         }
     }
 }
